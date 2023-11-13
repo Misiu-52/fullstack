422cd86fa7fd757ab5483cdc96ed336a30fe882a
Michał Kozłowski <kozlowskimichal52@gmail.com>
2023-11-13 11:49:47 +0100

    updt

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -3,6 +3,7 @@ import Note from './components/Note'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
   const [newNote, setNewNote] = useState('a new note...');
+  const [showAll, setShowAll] = useState(true);
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
@@ -19,11 +20,13 @@ const App = (props) => {
     setNewNote(event.target.value);
   }
 
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => <Note key={note.id} note={note} />)}
+        {notes.map(notesToShow => <Note key={note.id} note={note} />)}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
